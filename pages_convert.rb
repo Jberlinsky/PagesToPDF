844544156d8d5690d3095fee74e447a92bd5bd28
Jason Berlinsky <jberlinsky@gmail.com>
2013-02-13 12:12:04 -0500

    Pages conversion initial import

---
+++ pages_convert.rb
@@ -0,0 +1,20 @@
+require 'osx/cocoa'
+require 'pry'
+include OSX
+OSX.require_framework 'ScriptingBridge'
+
+`rm -rf /Users/Shared/CUPS-PDF/jberlinsky/*`
+
+pages = SBApplication.applicationWithBundleIdentifier_("com.apple.iWork.Pages")
+
+files = Dir["/Users/jberlinsky/Desktop/*.pages"]
+
+files.each do |path|
+  file_name = path.split("/").last.split(".").first
+  document = pages.open(path)
+  pages.print_printDialog_withProperties(path, false, {})
+  sleep 4
+  new_path = path.gsub("pages", "pdf")
+  `mv /Users/Shared/CUPS-PDF/jberlinsky/job_*.pdf #{new_path}`
+  document.closeSaving_savingIn(3, '')
+end
