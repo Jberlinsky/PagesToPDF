b98ecd4eacf844d04ce132596c5e06f802a47f95
Jason Berlinsky <jberlinsky@gmail.com>
2013-02-13 12:24:55 -0500

    Quote around paths

--- pages_convert.rb
+++ pages_convert.rb
@@ -12,11 +12,11 @@ puts "Converting #{files.count} files..."
 
 files.each do |path|
   file_name = path.split("/").last.split(".").first
-  document = pages.open(path)
-  pages.print_printDialog_withProperties(path, false, {})
+  document = pages.open("\"#{path}\"")
+  pages.print_printDialog_withProperties("\"#{path}\"", false, {})
   sleep 4
   new_path = path.gsub("pages", "pdf")
-  `mv /Users/Shared/CUPS-PDF/jberlinsky/job_*.pdf #{new_path}`
+  `mv /Users/Shared/CUPS-PDF/jberlinsky/job_*.pdf "#{new_path}"`
   document.closeSaving_savingIn(3, '')
   puts "Processed #{file_name} -> #{new_path}"
 end
