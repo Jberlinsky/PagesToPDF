ecfe596a59fa07aa6027683d5f65a57a5b237866
Jason Berlinsky <jberlinsky@gmail.com>
2013-02-13 12:37:40 -0500

    Do not escape some paths

--- pages_convert.rb
+++ pages_convert.rb
@@ -16,8 +16,8 @@ puts "Converting #{files.count} files..."
 
 files.each do |path|
   file_name = path.split("/").last.split(".").first
-  document = pages.open(escape(path))
-  pages.print_printDialog_withProperties(escape(path), false, {})
+  document = pages.open(path)
+  pages.print_printDialog_withProperties(path, false, {})
   sleep 4
   new_path = path.gsub("pages", "pdf")
   `mv /Users/Shared/CUPS-PDF/jberlinsky/job_*.pdf #{escape(new_path)}`
