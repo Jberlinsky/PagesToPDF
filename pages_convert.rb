0a07f11904aa275c5817a20d74f5f0298a493c08
Jason Berlinsky <jberlinsky@gmail.com>
2013-02-13 12:35:50 -0500

    Add escape sequences to paths

--- pages_convert.rb
+++ pages_convert.rb
@@ -5,6 +5,10 @@ OSX.require_framework 'ScriptingBridge'
 
 `rm -rf /Users/Shared/CUPS-PDF/jberlinsky/*`
 
+def escape(path)
+  path.gsub(" ", '\ ')
+end
+
 pages = SBApplication.applicationWithBundleIdentifier_("com.apple.iWork.Pages")
 
 files = Dir["/Volumes/Share/Client_List/**/*.pages"]
@@ -12,11 +16,11 @@ puts "Converting #{files.count} files..."
 
 files.each do |path|
   file_name = path.split("/").last.split(".").first
-  document = pages.open("\"#{path}\"")
-  pages.print_printDialog_withProperties("\"#{path}\"", false, {})
+  document = pages.open(escape(path))
+  pages.print_printDialog_withProperties(escape(path), false, {})
   sleep 4
   new_path = path.gsub("pages", "pdf")
-  `mv /Users/Shared/CUPS-PDF/jberlinsky/job_*.pdf "#{new_path}"`
+  `mv /Users/Shared/CUPS-PDF/jberlinsky/job_*.pdf #{escape(new_path)}`
   document.closeSaving_savingIn(3, '')
-  puts "Processed #{file_name} -> #{new_path}"
+  puts "Processed #{escape file_name} -> #{escape(new_path)}"
 end
